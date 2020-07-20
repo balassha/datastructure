a578d641f77158d6466cead870e1ae86c18b2d1a
balassha <63593969+balassha@users.noreply.github.com>
2020-07-20 11:57:08 +0530

    Create linkedlist.go

---
+++ linkedlist/linkedlist.go
@@ -0,0 +1,122 @@
+package main
+
+import(
+	"fmt"
+)
+
+type node struct{
+	next *node
+	data interface{}
+}
+
+type linkedList struct{
+	head *node
+	len int
+}
+
+func (l *linkedList) add(n *node){
+	if l.head == nil {
+		l.head = n
+		l.len++
+	}else{
+		t:= l.head
+		for t.next != nil {
+			t = t.next
+		}
+		t.next = n
+		l.len++
+	}
+}
+
+func (l *linkedList)print(){
+	t:=l.head
+	for t!=nil{
+		fmt.Printf("Data : %v\n",t.data)
+		t = t.next
+	}
+}
+
+func (l *linkedList)middle(){
+	var s,f *node
+	if l != nil && l.head != nil{
+		s = l.head
+		f = l.head
+	}
+	for f !=nil && f.next != nil {
+		s=s.next
+		f=f.next.next
+	}
+	if f.next != nil {
+		fmt.Printf("Middle: %v & %v\n",s.data,s.next.data)
+	}else{
+		fmt.Printf("Middle: %v\n",s.data)
+	}
+}
+
+func (l *linkedList) reverse(){
+	var c,p,n *node
+	c = l.head
+	for c!=nil{
+		n = c.next
+		if n==nil{
+			l.head = c
+		}
+		c.next=p
+		p=c
+		c=n
+	}
+}
+
+func (l *linkedList) restore(){
+	if l == nil {
+        return
+    }
+    n := l.head
+    for n != nil {
+        if n.data.(int) % 2 == 0 {
+            pt := n
+            var t linkedList
+            for n.data.(int) %2 == 0 {
+				t.add(&node{data:n.data,})
+				if n.next==nil{
+					break
+				}else{
+					n=n.next
+				}
+            }
+            t.reverse()
+            tp:=t.head
+            for tp != nil && pt != nil{
+                pt.data = tp.data
+                tp = tp.next
+                pt = pt.next
+			}
+			if n.next==nil{
+				break
+			}
+		}else{
+			if n.next==nil{
+				break
+			}else{
+				n=n.next
+			}
+		}
+
+		}
+}
+
+func main(){
+	fmt.Println("In Main")
+
+	in := [9]int{2,18,24,3,5,7,9,6,12}
+	var l linkedList
+	for _,v := range in{
+		l.add(&node{data:v,})
+	}
+	fmt.Println(l.len)
+	l.print()
+	l.middle()
+	l.reverse()
+	l.restore()
+	l.print()
+}
